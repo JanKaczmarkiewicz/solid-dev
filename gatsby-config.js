cae954ec3735888c7a26e4ab95f3c02885500956
JanKaczmarkiewicz <positiveKiddo@gmail.com>
2021-04-08 19:00:20 +0200

    feat: add plugins for svg & styled-componts

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,14 @@
 module.exports = {
-    plugins: ['gatsby-plugin-typescript'],
+    plugins: [
+        'gatsby-plugin-typescript',
+        'gatsby-plugin-styled-components',
+        {
+            resolve: 'gatsby-plugin-react-svg',
+            options: {
+                rule: {
+                    include: /assets/,
+                },
+            },
+        },
+    ],
 }
