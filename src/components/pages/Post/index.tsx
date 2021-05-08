31f14c8da9f63c12a126499c7ddd8aafe4affddf
JanKaczmarkiewicz <positiveKiddo@gmail.com>
2021-05-08 15:15:39 +0200

    feat: handle isDarkMode

--- src/components/pages/Post/index.tsx
+++ src/components/pages/Post/index.tsx
@@ -6,6 +6,7 @@ import { PageProps } from 'gatsby'
 import Header from '../../molecules/Header'
 import CodeHighlighter from '../../atom/CodeHighlighter'
 import { PostContainer, PostParagraph, PostTitle } from './styled'
+import Tags from '../../atom/Tags'
 
 const components: MDXProviderComponentsProp = {
     p: PostParagraph,
@@ -18,13 +19,15 @@ const components: MDXProviderComponentsProp = {
 const Post = ({ data: { mdx } }: PageProps<GatsbyTypes.PostQuery>) => {
     const title = mdx?.frontmatter?.title
     const body = mdx?.body || ''
+    const tags = mdx?.frontmatter?.tags || []
+    const isDarkMode = mdx?.frontmatter?.isDarkMode ?? false
 
     const imageData =
         mdx?.frontmatter?.featuredImage?.childImageSharp?.gatsbyImageData
 
     return (
         <>
-            <Header withShadow={false} variant="dark" />
+            <Header withShadow={false} isDarkMode={isDarkMode} />
 
             {imageData && (
                 <GatsbyImage
@@ -36,6 +39,8 @@ const Post = ({ data: { mdx } }: PageProps<GatsbyTypes.PostQuery>) => {
 
             <PostContainer>
                 <PostTitle>{title}</PostTitle>
+
+                <Tags tags={tags} />
 
                 <MDXProvider components={components}>
                     <MDXRenderer>{body}</MDXRenderer>
