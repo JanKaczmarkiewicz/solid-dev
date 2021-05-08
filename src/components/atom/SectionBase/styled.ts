cf9698cda1dbe05d08320696aad7371ba7accddf
JanKaczmarkiewicz <positiveKiddo@gmail.com>
2021-05-08 15:13:12 +0200

    feat: tablet max width for  SectionContainer

--- src/components/atom/SectionBase/styled.ts
+++ src/components/atom/SectionBase/styled.ts
@@ -1,8 +1,13 @@
 import styled from 'styled-components'
+import mediaQuery from '../../../styles/mediaQuery'
 import spacing from '../../../styles/spacing'
 
 export const SectionContainer = styled.div`
     max-width: ${spacing(72)};
     padding: 0 ${spacing(1)};
     margin: 0 auto;
+
+    @media ${mediaQuery.tablet} {
+        max-width: ${spacing(60)};
+    }
 `
